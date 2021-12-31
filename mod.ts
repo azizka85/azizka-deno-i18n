f74358f67fec3ebd1af3aeaf470f5f072d40f1a3
Aziz Kudaikulov <aziz.kudaikulov@gmail.com>
2021-12-31 19:16:48 +0600

    fix

--- mod.ts
+++ mod.ts
@@ -1,1 +1,9 @@
 export { Translator } from './src/translator.ts';
+
+export * from './src/utils.ts';
+
+export type { ContextOptions } from './src/data/context-options.ts';
+export type { DataOptions } from './src/data/data-options.ts';
+export type { FormattingContext } from './src/data/formatting-context.ts';
+export type { Values } from './src/data/values.ts';
+
